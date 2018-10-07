1161b7000f86b0d57ae898f3cd0c0f048dd5e861
gxagxagxa <gxagxagxa@me.com>
2018-10-07 20:10:25 +0800

    add recursive_update() func

--- dayu_job_center/src/util.py
+++ dayu_job_center/src/util.py
@@ -5,8 +5,15 @@ __author__ = 'andyguo'
 
 import sys
 
+
 def tprint(msg):
     """like print, but won't get newlines confused with multiple threads"""
     sys.stdout.write(str(msg) + '\n')
     sys.stdout.flush()
 
+
+def recursive_update(origin_dict, new_dict):
+    for k, v in new_dict.items():
+        if isinstance(v, dict) and isinstance(origin_dict.get(k, None), dict):
+            recursive_update(origin_dict[k], v)
+        origin_dict[k] = v
